220c3ba76927fc1ac1cc713d2590ead4134d3682
SAGESSE <gdmmyzc@163.com>
2024-03-05 02:27:16 +0800

    Add Neo Forge support

--- loom/src/main/groovy/moe/plushie/armourers_workshop/loom/MixinConfigTask.groovy
+++ loom/src/main/groovy/moe/plushie/armourers_workshop/loom/MixinConfigTask.groovy
@@ -1,6 +1,7 @@
 package moe.plushie.armourers_workshop.loom
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.Transformer
 import org.gradle.api.tasks.OutputFiles
 import org.gradle.api.tasks.TaskAction
 
@@ -28,22 +29,10 @@ class MixinConfigTask extends DefaultTask {
     }
 
     def attach() {
-        if (project.name == "forge") {
-            forge()
-        }
-        if (project.name == "fabric") {
-            fabric()
-        }
-    }
-
-    def forge() {
-        // when we switch the minecraft, the mixin will changes,
-        // so we need find all active mixins in the project,
-        // and then add all mixins name to manifest.
-        project.loom.forge { api ->
-            outputFiles.forEach {
-                api.mixinConfig it.name
-            }
+        switch (project.properties.get("loom.platform")) {
+            case "fabric": return fabric()
+            case "forge": return forge()
+            case "neoforge": return neoforge()
         }
     }
 
@@ -59,15 +48,51 @@ class MixinConfigTask extends DefaultTask {
                 }
                 mixins += "\"${it.name}\""
             }
-            replaceAll it, /(?sim)("mixins"\s*:\s*\[\s*)(.+?)(\s*\])/, /$1/ + mixins + /$3/
+            replaceAll(it) {
+                def results = it
+                results = results.replaceAll(/(?sim)("mixins"\s*:\s*\[\s*)(.+?)(\s*\])/, /$1/ + mixins + /$3/)
+                return results;
+            }
         }
     }
+
+    def forge() {
+        // when we switch the minecraft, the mixin will changes,
+        // so we need find all active mixins in the project,
+        // and then add all mixins name to manifest.
+        project.loom.forge { api ->
+            outputFiles.forEach {
+                api.mixinConfig it.name
+            }
+        }
+    }
+
+    def neoforge() {
+        // when we switch the minecraft, the mixin will changes,
+        // so we need find all active mixins in the project,
+        // and then add all mixins name to manifest.
+        project.tasks["processResources"].filesMatching("META-INF/mods.toml") {
+            def mixins = "\n\n"
+            outputFiles.forEach {
+                mixins += "[[mixins]]\nconfig = \"${it.name}\"\n\n"
+            }
+            replaceAll(it) {
+                def results = it
+                results = results.replaceAll(/(?sim)(^\s*modId\s*=\s*")forge(".+)/, /$1/ + "neoforge" + /$2/)
+                results = results.replaceAll(/(?sim)(^\s*mandatory\s*=\s*)true/, /type = "required"/)
+                results = results.replaceAll(/(?sim)(^\s*mandatory\s*=\s*)false/, /type = "optional"/)
+                results = results + mixins
+                return results
+            }
+        }
+    }
+
 
     @TaskAction
     def main() {
     }
 
-    static def replaceAll(details, regex, value) {
+    static def replaceAll(details, Transformer<String, String> transformer) {
         def results = ""
         details.filter {
             if (results.isEmpty()) {
@@ -80,7 +105,7 @@ class MixinConfigTask extends DefaultTask {
             }
         }
         details.filter {
-            results.replaceAll(regex, value)
+            transformer.transform(results)
         }
     }
 }
